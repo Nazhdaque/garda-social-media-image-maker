ac45772b55478044cceff911ab7c85867cfeb212
nazhdaque <nazhdaque@gmail.com>
2024-08-20 10:12:05 +0300

    split into two slides

--- js/importHTML.js
+++ js/importHTML.js
@@ -1,8 +1,6 @@
 import slide_1 from "../html/slide-1.html?raw";
 import slide_2 from "../html/slide-2.html?raw";
-import slide_3 from "../html/slide-3.html?raw";
 
 const slides = document.querySelector(".main-content");
 slides.insertAdjacentHTML("beforeend", slide_1);
 slides.insertAdjacentHTML("beforeend", slide_2);
-slides.insertAdjacentHTML("beforeend", slide_3);
